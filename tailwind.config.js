94e8207c8a137d9fc1288a41b9660b11b3bcb1cc
mohamedamin12 <aboamin909080@gmail.com>
2024-10-23 19:58:05 +0300

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        sm:"576px",
+        md:"768px",
+        lg:"992px",
+        xl:"1200px",
+      }
+    },
   },
   plugins: [],
 }
